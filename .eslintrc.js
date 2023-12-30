260d0adcae5859f5f8ce90f639090752f8c9d832
Marcin Butora <marcin.butora@vattenfall.com>
2023-12-30 21:58:18 +0100

    initial commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,5 @@
+const fs = require("fs");
+
 module.exports = {
   root: true,
   env: {
@@ -14,5 +16,6 @@ module.exports = {
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "prettier/prettier": ["warn", fs.readFileSync(".prettierrc")],
   },
 };
